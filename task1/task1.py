5ac801220cff00eac8f8a724cbb60104dc4d0410
Nenarr <163518632+Nenarr@users.noreply.github.com>
2024-10-03 19:28:07 +0400

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,16 @@
+import sys
 
+
+def Solution(*args):
+    lst = []
+    i = 1
+    while True:
+        lst.append(i)
+        i = 1 + (i + b - 2) % a
+        if i == 1:
+            return lst
+
+
+if __name__ == '__main__':
+    a, b = int(sys.argv[1]), int(sys.argv[2])
+    print(''.join(map(str, Solution(a, b))))
